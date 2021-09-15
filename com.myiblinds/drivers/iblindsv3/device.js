5bdd2b156f3d4ec43f7f15b4b4862295ff825885
chancehiblinds <chance@habhomeintel.com>
2021-09-15 11:36:33 -0500

    Convert device.js to ZWaveNode Extend, TODO:> Register Report Listeners, SetConfigurationParams

--- com.myiblinds/drivers/iblindsv3/device.js
+++ com.myiblinds/drivers/iblindsv3/device.js
@@ -1,34 +1,73 @@
 'use strict';
 
-const Homey= require('homey');
+const {ZwaveDevice} = require('homey-zwavedriver');
 
-class MyDevice extends Homey.Device {
+class iblindsV3 extends ZwaveDevice {
 
   /**
    * onInit is called when the device is initialized.
    */
-  async onInit() {
+  async onNodeInit() {
     this.log('iblinds V3 has been initialized');
-  }
+    this.enableDebug();
 
-  /**
-   * onAdded is called when the user adds the device, called just after pairing.
-   */
-  async onAdded() {
-    this.log('iblinds V3 has been added');
-  }
+    //print node info to console
+    this.printNode();
+    this.printNodeSummary();
+
+    /**
+     * Register Capabilities:
+     * 
+     * "measure_battery" = "BATTERY",
+     * "onoff" = "SWITCH_BINARY",
+     * "windowcoverings_tilt_down",
+     * "windowcoverings_tilt_set" = "SWITCH_MULTILEVEL",
+     * "windowcoverings_tilt_up"
+     */
+
+    this.registerCapability('measure_battery','BATTERY');
+    this.registerCapability('onoff', 'SWITCH_BINARY');
+    this.registerCapability('windowcoverings_tilt_set','SWITCH_MULTILEVEL');
+
+
+    /**
+     * Report Listeners
+     * -SWITCH_BINARY
+     *   -SWITCH_BINARY_REPORT
+     * -SWITCH_MULTILEVEL
+     *   -SWITCH_MULTILEVEL_REPORT
+     */
+    this.registerReportListener(
+      'SWITCH_BINARY',
+      'SWITCH_BINARY_REPORT',
+      (rawReport, parsedReport) => {
+        console.log('registerReportListener', rawReport, parsedReport);
+      },
+    );
+
+/**
+* 
+ * TODO this.configurationSet({id: 'Setting id'}, #value)
+ * this.configurationSet({index:1, size: 1}, #value)
+ */
+
+  } //OnNodeInit
+
 
   /**
    * onSettings is called when the user updates the device's settings.
    * @param {object} event the onSettings event data
    * @param {object} event.oldSettings The old settings object
    * @param {object} event.newSettings The new settings object
-   * @param {string[]} event.changedKeys An array of keys changed since the previous version
+   * @param {string[]} event.changedKeysArr An array of keys changed since the previous version
    * @returns {Promise<string|void>} return a custom message that will be displayed
    */
-  async onSettings({ oldSettings, newSettings, changedKeys }) {
+  async onSettings({ oldSettings, newSettings, changedKeysArr }) {
+    await super.onSettings(oldSettings, newSettings, changedKeysArr);
     this.log('iblinds V3 settings where changed');
+    return 'Settings Updated';
   }
+
 
   /**
    * onRenamed is called when the user updates the device's name.
@@ -48,4 +87,4 @@ class MyDevice extends Homey.Device {
 
 }
 
-module.exports = MyDevice;
+module.exports = iblindsV3;
